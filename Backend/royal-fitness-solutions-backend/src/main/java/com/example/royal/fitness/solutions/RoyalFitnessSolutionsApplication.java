eb8fdacc7b1034e457708a0a396f5765f25082ac
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-10-06 08:23:34 +0200

    project initialization

---
+++ Backend/royal-fitness-solutions-backend/src/main/java/com/example/royal/fitness/solutions/RoyalFitnessSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.royal.fitness.solutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalFitnessSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalFitnessSolutionsApplication.class, args);
+	}
+
+}
---
+++ Backend/royal-fitness-solutions-backend/src/test/java/com/example/royal/fitness/solutions/RoyalFitnessSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.royal.fitness.solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalFitnessSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
