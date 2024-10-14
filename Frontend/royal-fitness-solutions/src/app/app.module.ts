1837017300235c4612c368d579a0c2e55b30eb04
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-10-14 08:43:28 +0200

    few pages initialized

--- Frontend/royal-fitness-solutions/src/app/app.module.ts
+++ Frontend/royal-fitness-solutions/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthenticationFormComponent } from './forms/authentication-form/authentication-form.component';
+import { QueryFormComponent } from './forms/query-form/query-form.component';
+import { ExerciseFormComponent } from './forms/exercise-form/exercise-form.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MealPlansComponent } from './pages/meal-plans/meal-plans.component';
+import { TrainingPlansComponent } from './pages/training-plans/training-plans.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthenticationFormComponent,
+    QueryFormComponent,
+    ExerciseFormComponent,
+    HomePageComponent,
+    MealPlansComponent,
+    TrainingPlansComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/royal-fitness-solutions/src/app/pages/meal-plans/meal-plans.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-plans',
+  templateUrl: './meal-plans.component.html',
+  styleUrls: ['./meal-plans.component.scss']
+})
+export class MealPlansComponent {
+
+}
