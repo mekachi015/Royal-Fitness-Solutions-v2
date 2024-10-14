717b7e09d547be1ce586b3c99b284bfda44d1cba
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-10-14 08:56:38 +0200

    added routes for home, traing, meals and contact

--- Frontend/royal-fitness-solutions/src/app/app-routing.module.ts
+++ Frontend/royal-fitness-solutions/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { MealPlansComponent } from './pages/meal-plans/meal-plans.component';
+import { TrainingPlansComponent } from './pages/training-plans/training-plans.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomePageComponent},
+  {path: 'meals', component:MealPlansComponent},
+  {path: 'training', component:TrainingPlansComponent},
+  {path: 'contact', component:ContactPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
