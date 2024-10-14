5e4e2ef45bbf4166e3ad82929708b0f7e84adeda
mekachi015 <141609950+mekachi015@users.noreply.github.com>
2024-10-14 09:27:15 +0200

    navigation-bar implemented

--- Frontend/royal-fitness-solutions/src/app/components/navigation-bar/navigation-bar.component.ts
+++ Frontend/royal-fitness-solutions/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.scss']
 })
 export class NavigationBarComponent {
+  menuOpen: boolean = false;
+  currentRoute: string = '';
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url;
+      }
+    });
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  goToHome() {
+    this.router.navigate(['/home']);
+    this.toggleMenu();  // Close the menu after navigation
+  }
+
+  goToMeals() {
+    this.router.navigate(['/meals']);
+    this.toggleMenu();
+  }
+
+  goToTraining() {
+    this.router.navigate(['/training']);
+    this.toggleMenu();
+  }
+
+  goToContact() {
+    this.router.navigate(['/contact']);
+    this.toggleMenu();
+  }
 }
